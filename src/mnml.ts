bede02dd5bdccbd756c79d79492a99253968a1e4
Dan Ryan (GitHub Noreply Key) <dryan@users.noreply.github.com>
2022-02-11 13:36:35 -0500

    ♻️  setup code so the global namespace isn't polluted

--- src/mnml.ts
+++ src/mnml.ts
@@ -1,251 +1,245 @@
-export const parser = new DOMParser();
-
-const isInstance = (thing: any, kind: any, param: string): void => {
-  if (!(thing instanceof kind)) {
-    throw new Error(
-      `Expected ${param} to be a ${kind && kind.constructor && kind.constructor.name}`
-    );
-  }
-};
-
-const isType = (thing: any, kind: string, param: string): void => {
-  if (typeof thing !== kind) {
-    throw new Error(`Expected ${param} to be a ${kind}`);
-  }
-};
-
-const createElementCache: { [key: string]: HTMLElement } = {};
-
-export const createElement = (tagName: string): HTMLElement => {
-  if (!createElementCache[tagName]) {
-    createElementCache[tagName] = document.createElement(tagName);
-  }
-  return createElementCache[tagName].cloneNode() as HTMLElement;
-};
-
-export const createHTML = (content: string): HTMLElement => {
-  const template = createElement("template") as HTMLTemplateElement;
-  isInstance(template, HTMLTemplateElement, "template");
-  template.innerHTML = content.trim();
-  const output = template.content.firstChild as HTMLElement;
-  isInstance(output, HTMLElement, "output");
-  return output;
-};
-
-export const find = (
-  elem: HTMLElement | string,
-  selector: string
-): HTMLElement | null => {
-  if (typeof elem === "string") {
-    selector = elem;
-    elem = document.documentElement;
-  }
-  return elem.querySelector(selector);
-};
-
-export const findAll = (
-  elem: HTMLElement | string,
-  selector: string
-): HTMLElement[] => {
-  if (typeof elem === "string") {
-    selector = elem;
-    elem = document.documentElement as HTMLElement;
-  }
-  return [...elem.querySelectorAll(selector)].map((elem) => elem as HTMLElement);
-};
-
-export const findParent = (elem: HTMLElement, selector: string): HTMLElement | null => {
-  let parent = elem.parentElement;
-  if (!parent || !parent.matches) {
-    return null;
-  }
-  while (!parent.matches(selector)) {
-    parent = parent.parentElement;
-    if (!parent || !parent.matches) {
-      parent = null;
-      break;
-    }
-  }
-  return parent;
-};
-
-export const findParents = (elem: HTMLElement, selector: string): HTMLElement[] => {
-  const parents = [];
-  let parent: HTMLElement | null = elem;
-  while ((parent = parent.parentElement)) {
-    if (parent && parent.matches && parent.matches(selector)) {
-      parents.push(parent);
-    }
-  }
-  return parents;
-};
-
-export const _findEventTarget = (ev: Event, selector: string): HTMLElement => {
-  return (ev.composedPath &&
-    ev
-      .composedPath()
-      .filter((elem) => {
-        const el = elem as HTMLElement;
-        return el.matches && el.matches(selector);
-      })
-      .shift()) as HTMLElement;
-};
-
 export type EventCallback = (ev?: Event, match?: HTMLElement) => void;
-
-const _loadListener = (callback: EventCallback, priority?: number): any => {
-  priority = priority || 10;
-  if (_loadListener.loaded) {
-    return callback();
-  }
-  _loadListener.queue.push([callback, priority]);
-};
-_loadListener.queue = [] as Array<[EventCallback, number]>;
-_loadListener.loaded = false;
-window.addEventListener("load", () => {
-  _loadListener.queue = _loadListener.queue.sort((a, b) => a[1] - b[1]);
-  while (_loadListener.queue.length) {
-    const cb = _loadListener.queue.shift();
-    if (cb) {
-      cb[0]();
-    }
-  }
-  _loadListener.loaded = true;
-});
-
-const _readyListener = (callback: EventCallback, priority?: number): any => {
-  priority = priority || 10;
-  if (_readyListener.loaded) {
-    return callback();
-  }
-  _readyListener.queue.push([callback, priority]);
-};
-_readyListener.queue = [] as Array<[EventCallback, number]>;
-_readyListener.loaded = false;
-document.addEventListener("DOMContentLoaded", () => {
-  _readyListener.queue = _readyListener.queue.sort((a, b) => a[1] - b[1]);
-  while (_readyListener.queue.length) {
-    const cb = _readyListener.queue.shift();
-    if (cb) {
-      cb[0]();
-    }
-  }
-  _readyListener.loaded = true;
-});
-
-export const listen = (
-  eventName: string,
-  selector: string | number | EventCallback,
-  callback: EventCallback,
-  replace: boolean = true
-): void => {
-  if (eventName === "load") {
-    if (typeof selector === "function") {
-      return _loadListener(selector);
-    }
-    if (typeof selector !== "number") {
-      throw new Error(
-        `Expected selector to be a number but was ${
-          (selector && selector.constructor && selector.constructor.name) || selector
-        }`
-      );
-    }
-    return _loadListener(callback, selector);
-  }
-
-  if (eventName === "ready") {
-    if (typeof selector === "function") {
-      return _readyListener(selector);
-    }
-    if (typeof selector !== "number") {
-      throw new Error(
-        `Expected selector to be a number but was ${
-          (selector && selector.constructor && selector.constructor.name) || selector
-        }`
-      );
-    }
-    return _readyListener(callback, selector);
-  }
-
-  if (typeof selector !== "string") {
-    throw new Error(
-      `Expected selector to be a string but was ${
-        (selector && selector.constructor && selector.constructor.name) || selector
-      }`
-    );
-  }
-
-  if (typeof listen.cache[eventName] === "undefined") {
-    listen.cache[eventName] = {} as { [key: string]: EventCallback[] };
-  }
-
-  if (!(selector in (listen.cache[eventName] as object))) {
-    listen.cache[eventName][selector] = [];
-  }
-  if (replace) {
-    listen.cache[eventName][selector] = [];
-  }
-
-  listen.cache[eventName][selector].push(callback);
-
-  if (listen.registeredEvents.indexOf(eventName) === -1) {
-    listen.registeredEvents.push(eventName);
-
-    document.addEventListener(eventName, (ev) => {
-      Object.keys(listen.cache[eventName]).map((s) => {
-        const match = _findEventTarget(ev, s);
-        if (match) {
-          listen.cache[eventName][s].map((cb: EventCallback) => {
-            cb(ev, match);
-          });
-        }
-      });
-    });
-  }
-};
-listen.cache = {} as { [key: string]: { [key: string]: EventCallback[] } };
-listen.registeredEvents = [] as string[];
-
-const uuid = (): string => {
-  return "10000000-1000-4000-8000-100000000000".replace(/[018]/g, (c: string) =>
-    (
-      parseInt(c, 10) ^
-      (crypto.getRandomValues(new Uint8Array(1))[0] & (15 >> (parseInt(c, 10) / 4)))
-    ).toString(16)
-  );
-};
 
 export interface ParamsObject {
   [key: string]: string | string[];
 }
 
-export const params = (str: string = document.location.search): ParamsObject => {
-  str = str.replace(/(^\?)/, "");
-  if (!str) {
-    return {};
-  } else if (params.cache[str]) {
-    return params.cache[str];
-  }
-  const _params = new URLSearchParams(str);
-  const obj: ParamsObject = {};
-  [..._params.entries()].map((entry) => {
-    const [key, value] = entry;
-    if (Object.keys(obj).includes(key) && !Array.isArray(obj[key])) {
-      // if there's already a property here and the value isn't an array, make it one
-      obj[key] = [obj[key] as string] as string[];
-    }
-    if (Array.isArray(obj[key])) {
-      (obj[key] as string[]).push(value);
-    } else {
-      obj[key] = value as string;
-    }
-  });
-  return obj;
-};
-params.cache = {} as { [key: string]: any };
+export const mnml = (() => {
+  const parser = new DOMParser();
 
-export default {
-  mnml: {
+  const isInstance = (thing: any, kind: any, param: string): void => {
+    if (!(thing instanceof kind)) {
+      throw new Error(
+        `Expected ${param} to be a ${kind && kind.constructor && kind.constructor.name}`
+      );
+    }
+  };
+
+  const isType = (thing: any, kind: string, param: string): void => {
+    if (typeof thing !== kind) {
+      throw new Error(`Expected ${param} to be a ${kind}`);
+    }
+  };
+
+  const createElementCache: { [key: string]: HTMLElement } = {};
+
+  const createElement = (tagName: string): HTMLElement => {
+    if (!createElementCache[tagName]) {
+      createElementCache[tagName] = document.createElement(tagName);
+    }
+    return createElementCache[tagName].cloneNode() as HTMLElement;
+  };
+
+  const createHTML = (content: string): HTMLElement => {
+    const template = createElement("template") as HTMLTemplateElement;
+    isInstance(template, HTMLTemplateElement, "template");
+    template.innerHTML = content.trim();
+    const output = template.content.firstChild as HTMLElement;
+    isInstance(output, HTMLElement, "output");
+    return output;
+  };
+
+  const find = (elem: HTMLElement | string, selector: string): HTMLElement | null => {
+    if (typeof elem === "string") {
+      selector = elem;
+      elem = document.documentElement;
+    }
+    return elem.querySelector(selector);
+  };
+
+  const findAll = (elem: HTMLElement | string, selector: string): HTMLElement[] => {
+    if (typeof elem === "string") {
+      selector = elem;
+      elem = document.documentElement as HTMLElement;
+    }
+    return [...elem.querySelectorAll(selector)].map((elem) => elem as HTMLElement);
+  };
+
+  const findParent = (elem: HTMLElement, selector: string): HTMLElement | null => {
+    let parent = elem.parentElement;
+    if (!parent || !parent.matches) {
+      return null;
+    }
+    while (!parent.matches(selector)) {
+      parent = parent.parentElement;
+      if (!parent || !parent.matches) {
+        parent = null;
+        break;
+      }
+    }
+    return parent;
+  };
+
+  const findParents = (elem: HTMLElement, selector: string): HTMLElement[] => {
+    const parents = [];
+    let parent: HTMLElement | null = elem;
+    while ((parent = parent.parentElement)) {
+      if (parent && parent.matches && parent.matches(selector)) {
+        parents.push(parent);
+      }
+    }
+    return parents;
+  };
+
+  const _findEventTarget = (ev: Event, selector: string): HTMLElement => {
+    return (ev.composedPath &&
+      ev
+        .composedPath()
+        .filter((elem) => {
+          const el = elem as HTMLElement;
+          return el.matches && el.matches(selector);
+        })
+        .shift()) as HTMLElement;
+  };
+
+  const _loadListener = (callback: EventCallback, priority?: number): any => {
+    priority = priority || 10;
+    if (_loadListener.loaded) {
+      return callback();
+    }
+    _loadListener.queue.push([callback, priority]);
+  };
+  _loadListener.queue = [] as Array<[EventCallback, number]>;
+  _loadListener.loaded = false;
+  window.addEventListener("load", () => {
+    _loadListener.queue = _loadListener.queue.sort((a, b) => a[1] - b[1]);
+    while (_loadListener.queue.length) {
+      const cb = _loadListener.queue.shift();
+      if (cb) {
+        cb[0]();
+      }
+    }
+    _loadListener.loaded = true;
+  });
+
+  const _readyListener = (callback: EventCallback, priority?: number): any => {
+    priority = priority || 10;
+    if (_readyListener.loaded) {
+      return callback();
+    }
+    _readyListener.queue.push([callback, priority]);
+  };
+  _readyListener.queue = [] as Array<[EventCallback, number]>;
+  _readyListener.loaded = false;
+  document.addEventListener("DOMContentLoaded", () => {
+    _readyListener.queue = _readyListener.queue.sort((a, b) => a[1] - b[1]);
+    while (_readyListener.queue.length) {
+      const cb = _readyListener.queue.shift();
+      if (cb) {
+        cb[0]();
+      }
+    }
+    _readyListener.loaded = true;
+  });
+
+  const listen = (
+    eventName: string,
+    selector: string | number | EventCallback,
+    callback: EventCallback,
+    replace: boolean = true
+  ): void => {
+    if (eventName === "load") {
+      if (typeof selector === "function") {
+        return _loadListener(selector);
+      }
+      if (typeof selector !== "number") {
+        throw new Error(
+          `Expected selector to be a number but was ${
+            (selector && selector.constructor && selector.constructor.name) || selector
+          }`
+        );
+      }
+      return _loadListener(callback, selector);
+    }
+
+    if (eventName === "ready") {
+      if (typeof selector === "function") {
+        return _readyListener(selector);
+      }
+      if (typeof selector !== "number") {
+        throw new Error(
+          `Expected selector to be a number but was ${
+            (selector && selector.constructor && selector.constructor.name) || selector
+          }`
+        );
+      }
+      return _readyListener(callback, selector);
+    }
+
+    if (typeof selector !== "string") {
+      throw new Error(
+        `Expected selector to be a string but was ${
+          (selector && selector.constructor && selector.constructor.name) || selector
+        }`
+      );
+    }
+
+    if (typeof listen.cache[eventName] === "undefined") {
+      listen.cache[eventName] = {} as { [key: string]: EventCallback[] };
+    }
+
+    if (!(selector in (listen.cache[eventName] as object))) {
+      listen.cache[eventName][selector] = [];
+    }
+    if (replace) {
+      listen.cache[eventName][selector] = [];
+    }
+
+    listen.cache[eventName][selector].push(callback);
+
+    if (listen.registeredEvents.indexOf(eventName) === -1) {
+      listen.registeredEvents.push(eventName);
+
+      document.addEventListener(eventName, (ev) => {
+        Object.keys(listen.cache[eventName]).map((s) => {
+          const match = _findEventTarget(ev, s);
+          if (match) {
+            listen.cache[eventName][s].map((cb: EventCallback) => {
+              cb(ev, match);
+            });
+          }
+        });
+      });
+    }
+  };
+  listen.cache = {} as { [key: string]: { [key: string]: EventCallback[] } };
+  listen.registeredEvents = [] as string[];
+
+  const uuid = (): string => {
+    return "10000000-1000-4000-8000-100000000000".replace(/[018]/g, (c: string) =>
+      (
+        parseInt(c, 10) ^
+        (crypto.getRandomValues(new Uint8Array(1))[0] & (15 >> (parseInt(c, 10) / 4)))
+      ).toString(16)
+    );
+  };
+
+  const params = (str: string = document.location.search): ParamsObject => {
+    str = str.replace(/(^\?)/, "");
+    if (!str) {
+      return {};
+    } else if (params.cache[str]) {
+      return params.cache[str];
+    }
+    const _params = new URLSearchParams(str);
+    const obj: ParamsObject = {};
+    [..._params.entries()].map((entry) => {
+      const [key, value] = entry;
+      if (Object.keys(obj).includes(key) && !Array.isArray(obj[key])) {
+        // if there's already a property here and the value isn't an array, make it one
+        obj[key] = [obj[key] as string] as string[];
+      }
+      if (Array.isArray(obj[key])) {
+        (obj[key] as string[]).push(value);
+      } else {
+        obj[key] = value as string;
+      }
+    });
+    return obj;
+  };
+  params.cache = {} as { [key: string]: any };
+
+  return {
     parser,
     createElement,
     createHTML,
@@ -256,5 +250,7 @@ export default {
     listen,
     uuid,
     params,
-  },
-};
+  };
+})();
+
+export default mnml;
